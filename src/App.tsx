ddaff88c50092e62d8b8e69a84e179dcf70eb024
TheElvace <anselmelvis62@gmail.com>
2025-06-21 06:06:19 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { ThemeProvider, CssBaseline } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { theme } from './theme';
-import HomePage from './pages/Home';
 import AllBriefs from './pages/AllBriefs';
 
 const queryClient = new QueryClient();
