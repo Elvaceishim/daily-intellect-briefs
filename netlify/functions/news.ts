95a4211bde3141e7df5fee04be138162fc8a6b2f
TheElvace <anselmelvis62@gmail.com>
2025-06-22 19:33:24 +0100

    commit

--- netlify/functions/news.ts
+++ netlify/functions/news.ts
@@ -1,4 +1,3 @@
-import { useState } from "react";
 const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
@@ -21,11 +20,11 @@ exports.handler = async function(event, context) {
   }
 };
 
-const handleClick = async (brief: { title: string }) => {
-  const response = await fetch(
-    `/.netlify/functions/news?query=${encodeURIComponent(brief.title)}`
-  );
-  const data = await response.json();
-  setNews(data.articles?.[0] || null); // Show the first article or null
-};
-const [news, setNews] = useState<any>(null);
+// const handleClick = async (brief: { title: string }) => {
+//   const response = await fetch(
+//     `/.netlify/functions/news?query=${encodeURIComponent(brief.title)}`
+//   );
+//   const data = await response.json();
+//   setNews(data.articles?.[0] || null); // Show the first article or null
+// };
+// const [news, setNews] = useState<any>(null);
