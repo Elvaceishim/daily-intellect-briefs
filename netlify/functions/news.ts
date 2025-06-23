96d5ad4cd2177806185aace67d507a5b3f4db1b4
TheElvace <anselmelvis62@gmail.com>
2025-06-23 16:31:02 +0100

    commit

--- netlify/functions/news.ts
+++ netlify/functions/news.ts
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 
 exports.handler = async function (event, context) {
   const query = event.queryStringParameters?.query || '';
-  const limit = event.queryStringParameters?.limit || 10;
+  const limit = event.queryStringParameters?.limit || 7;
   const apiKey = process.env.NEWS_API_KEY || '5121f0823678cd12355a12dcf26358ba';
 
   let url = `https://api.mediastack.com/v1/news?access_key=${apiKey}&countries=us&languages=en&limit=${limit}`;
